050f1f874361158aeb45af610f840e86195c1224
RinkalPatel <rinkal.patel@sanelib.com>
2016-09-22 10:02:44 +0530

    create console application

---
+++ LearningProgram/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LearningProgram;
+
+namespace LearningProgram
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+            }
+        }
+}

0962b6f5aa6046cbf6e56ec1fd41a016c6969697
RinkalPatel <rinkal.patel@sanelib.com>
2016-09-22 10:03:44 +0530

    Adding  inheritance assignments

---
+++ LearningProgram/Inheritance/Assignment.cs
@@ -0,0 +1,40 @@
+﻿namespace LearningProgram.Inheritance
+{
+    class A
+    {
+        //base class 
+        int a;
+        public A()
+        {
+
+        }
+        public void F()
+        {
+
+        }
+    }
+    class B : A    {
+        //subclass(inherits from A, extends A) 
+        //B inherits aand F(), it adds band G() 
+
+        //-----constructors are not inherited -----/ /
+
+        //class can only inherit from a class, not from a struct
+        int b;
+
+        public B()
+        {
+
+        }
+
+        public void G()
+        {
+
+        }
+    }
+
+    class C:B
+    {
+        
+    }
+}
